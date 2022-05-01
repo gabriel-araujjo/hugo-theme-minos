d0136b5c03be86e8fbd403dd200a61a415aa7dcf
Gabriel Araújo <gabriel140492@gmail.com>
2022-05-01 15:32:13 -0300

    Add doi

--- assets/js/cite.js
+++ assets/js/cite.js
@@ -29,9 +29,13 @@ const el = t => (content = []) => {
     return e;
 };
 
-const i = el('i');
+const italic = el('i');
 const b = el('b');
-const pag = p => p.split('-').join('–');
+const pag = p => {
+    const parts = p.split('-');
+    if (parts[0] === parts[1]) return parts[0];
+    else return parts.join('–');
+};
 const year = i => i[0];
 
 const isup = s => s.toUpperCase() === s;
@@ -45,13 +49,19 @@ const fmt = {
         const m = ['jan.', 'fev.', 'mar.', 'abr.', 'maio', 'jun.', 'jul.', 'ago.', 'set.', 'out.', 'nov.', 'dez.'];
         const dat = i => `${i[2]} ${m[i[1]-1]} ${i[0]}`;
         const nam = n => ap(`${n.family.toUpperCase()}, ${ini(n.given).join('. ')}`);
-        const auth = a => a.length > 3 ? [txt(nam(a[0])), i([txt(' et al.')])]: [txt(prd(a.map(n => nam(n)).join('; ')))];
-        return d => [
-            ...auth(d.author),
-            txt(` ${d.title}. `),
-            b([txt(d.journal)]),
-            txt(`, n. ${d.issue}, p. ${pag(d.page)}, ${dat(d.issued)}.`)
-        ];
+        const auth = a => a.length > 3 ? [txt(nam(a[0])), italic([txt(' et al.')])]: [txt(prd(a.map(n => nam(n)).join('; ')))];
+        return d => {
+            const now = new Date();
+            const doi = d.doi
+                ? ` Disponível em: <${d.doi}>. Acesso em: ${now.getDate()} ${m[now.getMonth()-1]} de ${now.getFullYear()}.`
+                : '';
+            return [
+                ...auth(d.author),
+                txt(` ${d.title}. `),
+                b([txt(d.journal)]),
+                txt(`, n. ${d.issue}, p. ${pag(d.page)}, ${dat(d.issued)}.${doi}`)
+            ];
+        };
     })(),
     apa: (() => {
         const nam = n => ap(`${n.family}, ${ini(n.given).join('. ')}`);
@@ -64,12 +74,15 @@ const fmt = {
                 default: return ap(nam(a[0])) + 'et al';
             }
         };
-        return d => [
-            txt(auth(d.author)),
-            txt(` (${year(d.issued)}). ${d.title}. `),
-            i([txt(d.journal)]),
-            txt(`, ${d.issue}, ${pag(d.page)}.`)
-        ];
+        return d => {
+            const doi = d.doi ? ` ${d.doi}` : '';
+            return [
+                txt(auth(d.author)),
+                txt(` (${year(d.issued)}). ${d.title}. `),
+                italic([txt(d.journal)]),
+                txt(`, ${d.issue}, ${pag(d.page)}.${doi}`)
+            ];
+        };
     })(),
     hvd: (() => {
         const nam = n => {
@@ -86,15 +99,18 @@ const fmt = {
                 case 0: return [];
                 case 1: return [txt(nam(a[0]))];
                 case 2: return [txt([...a.slice(0, -1).map(nam)].join(', ') + ' & ' + nam(a[a.length-1]))];
-                default: return [txt(nam(a[0]) + ' '), i([txt(' et al.')])];
+                default: return [txt(nam(a[0]) + ' '), italic([txt(' et al.')])];
             }
         };
-        return d => [
-            ...auth(d.author),
-            txt(` (${year(d.issued)}) ‘${d.title},’ `),
-            i([txt(d.journal)]),
-            txt(`, (${d.issue}), p${pag(d.page)}.`)
-        ];
+        return d => {
+            const doi = d.doi ? `, DOI:${d.doi.replace(/https?:\/\/doi\.org\//, '')}` : '';
+            return [
+                ...auth(d.author),
+                txt(` (${year(d.issued)}) ‘${d.title},’ `),
+                italic([txt(d.journal)]),
+                txt(`, (${d.issue}), p${pag(d.page)}${doi}.`)
+            ];
+        };
     })(),
     vcv: (() => {
         const dat = i => `${i[0]} ${meng[i[1]-1]} ${i[2].padStart(2, '0')}`;
@@ -107,10 +123,13 @@ const fmt = {
             return res;
         };
         const auth = a => a.map(nam).join(', ');
-        return d => [
-            txt(auth(d.author)),
-            txt(`. ${d.title}. ${d.journal}. ${dat(d.issued)}; (${d.issue}):${pag(d.page)}.`)
-        ];
+        return d => {
+            const doi = d.doi ? ` ${d.doi}.` : '';
+            return [
+                txt(auth(d.author)),
+                txt(`. ${d.title}. ${d.journal}. ${dat(d.issued)}; (${d.issue}):${pag(d.page)}.${doi}`)
+            ];
+        };
     })(),
     bbt: (() => {
         const nam = n => n.family + ', ' + n.given;
@@ -120,16 +139,22 @@ const fmt = {
             const i = /[.,:;!?/'’"”\])}]$/.test(s) ? -1 : s.length;
             return `{${s.slice(0, i)}}${s.slice(i)}`;
         };
+        const pages = p => {
+            const parts = p.split('-');
+            if (parts[0] == parts[1]) return parts[0];
+            else return parts.join('--');
+        };
         const bt = d => {
+            const doi = d.doi ? `
+    doi = {${d.doi}},` : '';
             const tp = d.title.split(/\s+/);
             return `@article{${d.author[0].family.split(/\s+/).join('')}${year(d.issued)}${cap(tp.find(s => s.length > 1))},
     author = {${auth(d.author)}},
     title = {${tp.reduce((res, w, i) => res + ' ' + (i && isup(w[0]) ? wrp(w): w), '').trim()}},
     journal = {${d.journal}},
     number = {${d.issue}},
-    year = {${year(d.issued)}},
-    month = {${meng[d.issued[1]].replace('.', '')} ${d.issued[2]}},
-    pages = {${d.page.split('-').join('--')}}
+    year = {${year(d.issued)}},${doi}
+    pages = {${pages(d.page)}}
 }`;
         };
         const code = d => {
